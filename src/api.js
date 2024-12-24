67ba790930ad5ebdb73da5563bb85b3547618108
f-piken <totpiken@outlook.com>
2024-12-24 23:53:19 +0700

    email-pemabayaran

--- src/api.js
+++ src/api.js
@@ -1,11 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-<<<<<<< HEAD
   baseURL: 'http://localhost:8000/api',
-=======
-  baseURL: 'http://localhost:8000/api', // Ganti dengan URL API Laravel kamu
->>>>>>> 5dca85bd (pendaftaran)
 });
 
 api.interceptors.request.use(
